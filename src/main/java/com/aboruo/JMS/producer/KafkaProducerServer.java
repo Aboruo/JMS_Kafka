2dbf12cc1f2d39c96c9f18dd8a8628e51a59c77c
zhaoxinbo <zhaoxinbo@cm-inv.com>
2018-04-13 20:07:45 +0800

    producer调整

--- src/main/java/com/aboruo/JMS/producer/KafkaProducerServer.java
+++ src/main/java/com/aboruo/JMS/producer/KafkaProducerServer.java
@@ -3,6 +3,7 @@ package com.aboruo.JMS.producer;
 import com.aboruo.JMS.constant.JmsKafkaEnum;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
